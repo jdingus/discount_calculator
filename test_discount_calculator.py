feb7db977a6cb3947629928372b02b03a5c9482e
Josh <josh.dingus@gmail.com>
2014-07-07 21:00:33 -0400

    testing built

--- test_discount_calculator.py
+++ test_discount_calculator.py
@@ -8,6 +8,26 @@ class DiscountCalculatorTests(unittest.TestCase):
 
         self.assertEqual(price,81)
 
+    def testZeroDiscount(self):
+        price = calculate_discount(100,0,.1)
+
+        self.assertEqual(price,90)
+
+    def testZeroDiscount2(self):
+        price = calculate_discount(100,.1,0)
+
+        self.assertEqual(price,90)
+
+    def testpriceNotNegative(self):
+        price = calculate_discount(100,1.5,5.)
+
+        self.assertGreater(price,0)
+
+    def testpriceNotBigger(self):
+    	initial_price = 100.
+        price = calculate_discount(initial_price,1.5,5.)
+
+        self.assertGreater(price,initial_price)
 
 if __name__ == '__main__':
 	unittest.main()
